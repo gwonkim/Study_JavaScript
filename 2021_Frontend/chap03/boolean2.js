ab0e89d992e25fe6593a49480d271fbac77d88dd
gwonkim <wo41896804@gmail.com>
2021-03-17 22:18:23 +0900

    3주차 수업 실습
    - 자료형

---
+++ 2021_Frontend/chap03/boolean2.js
@@ -0,0 +1,6 @@
+let a = undefined, b = null, c = 1234, d = NaN;
+
+console.log(a ? "true" : "false");
+console.log(b ? "true" : "false");
+console.log(c ? "true" : "false");
+console.log(d ? "true" : "false");
