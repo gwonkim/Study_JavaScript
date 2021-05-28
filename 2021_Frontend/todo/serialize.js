dbc7b091946f9203002574dcafbff21e7a81d723
gwonkim <wo41896804@gmail.com>
2021-05-28 23:21:02 +0900

    todo3
    JSON

---
+++ 2021_Frontend/todo/serialize.js
@@ -0,0 +1,4 @@
+let person = { name: "홍길동", age: 16 };
+
+let s = JSON.stringify(person);
+console.log(s);
---
+++ 2021_Frontend/todo/unserialize.js
@@ -0,0 +1,4 @@
+let s = '{"name":"홍길동","age":16}';
+
+let person = JSON.parse(s);
+console.log(person);
