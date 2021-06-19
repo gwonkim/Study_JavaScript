fc5fbb3ad162b564e8542cf3f904634ffd27700c
gwonkim <wo41896804@gmail.com>
2021-06-20 00:05:46 +0900

    practice7 : 월요일 수업 기말고사
    6/14

---
+++ 2021_Frontend/practice7/exam4/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+<h1>컴포넌트</h1>
+          <select v-model="c">
+        <option>green</option>
+        <option>red</option>
+        <option>blue</option>
+        <option selected>gray</option>
+      </select>
+    <br />
+    <Child :c='c' />
+  </div>
+</template>
+
+<script>
+import Child from './components/Child.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Child
+  },
+   data: function() {
+    return { 
+    c: 'gray'
+  };
+  },
+}
+</script>
+
+<style>
+div#app { padding: 30px; margin: 30px auto; max-width: 500px; 
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa;  font-size: 11pt; }
+    div#app div { margin-bottom: 20px; }
+    select { padding: 6px; margin-bottom: 10px;  width: 150px;}
+    span { display: inline-block; margin: 5px; padding: 8px 15px;
+            border: 1px solid black; }
+    button { padding: 0.5em 2em; margin-right: 10px;  }
+</style>
---
+++ 2021_Frontend/practice7/exam4/src/components/Child.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="str" :style="{color : c}">
+      <h2>Child 컴포넌트</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Child',
+   props: {
+       c: String,
+   },
+  data: function() {
+    return { 
+    a: [],
+  };
+  },
+}
+</script>
+
+<style>
+    div#app { padding: 30px; margin: 30px auto; max-width: 500px; 
+      border: 1px solid #ccc; box-shadow: 3px 3px 3px #aaa;  font-size: 11pt; }
+    div#app div { margin-bottom: 20px; }
+    input { padding: 6px; margin: 10px; }
+    div.str { padding: 20px; border: 1px solid black; color: gray   }
+    span { display: inline-block; margin: 5px; padding: 8px 15px;
+            border: 1px solid black; }
+    button { padding: 0.5em 2em; margin-right: 10px;  }
+  </style>
+
