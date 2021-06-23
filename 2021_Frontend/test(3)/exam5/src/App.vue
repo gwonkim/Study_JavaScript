d542e43702be2ec515c1d37f442dceb8bd66db2c
gwonkim <wo41896804@gmail.com>
2021-06-24 00:00:43 +0900

    exam(5)

---
+++ 2021_Frontend/test(3)/exam5/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <h1>카운터</h1>
+    <counter />
+  </div>
+</template>
+
+<script>
+import Counter from "./components/Counter.vue";
+
+export default {
+  name: "App",
+  components: {
+    Counter,
+  },
+};
+</script>
+
+<style>
+div#app {
+  padding: 30px;
+  margin: 30px auto;
+  max-width: 500px;
+  border: 1px solid #ccc;
+  box-shadow: 3px 3px 3px #aaa;
+  font-size: 11pt;
+}
+</style>
---
+++ 2021_Frontend/test(3)/exam5/src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="count">
+    <h2>{{ count }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Counter",
+  data: function () {
+    return {
+      count: 0,
+    };
+  },
+  mounted() {
+    let callback = () => {
+      this.count++;
+    };
+    this.timerId = setInterval(callback, 1000);
+  },
+};
+</script>
+
+<style>
+.count {
+  text-align: center;
+  width: 100px;
+  background: #ffa;
+  border: 1px solid #000;
+}
+</style>
+
