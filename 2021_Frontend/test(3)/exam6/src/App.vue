190012403a8bb510281035bddfe995f1ee9929ff
gwonkim <wo41896804@gmail.com>
2021-06-25 23:13:46 +0900

    exam(6)

---
+++ 2021_Frontend/test(3)/exam6/src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <h1>컴포넌트</h1>
+    <input type="text" v-model.number="size" />
+    <Child :size="size" />
+  </div>
+</template>
+
+<script>
+import Child from "./components/Child.vue";
+
+export default {
+  name: "App",
+  components: {
+    Child,
+  },
+  data: function () {
+    return {
+      size: 3,
+    };
+  },
+};
+</script>
+
+<style>
+div#app {
+  padding: 30px;
+  margin: 30px auto;
+  max-width: 600px;
+  border: 1px solid #ccc;
+  box-shadow: 3px 3px 3px #aaa;
+}
+input {
+  padding: 5px;
+  font-size: 20px;
+  width: 150px;
+  margin-bottom: 10px;
+}
+</style>
---
+++ 2021_Frontend/test(3)/exam6/src/components/Child.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="a">
+    <span v-for="(s, i) in size" :key="i">
+      {{ s }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Child",
+  props: {
+    size: Number,
+  },
+};
+</script>
+
+<style>
+div.a {
+  margin-bottom: 20px;
+  margin-top: 10px;
+  padding: 20px;
+  background: #ffa;
+  border: 1px solid black;
+}
+span {
+  display: inline-block;
+  margin: 10px;
+  padding: 10px 15px;
+  border: 1px solid black;
+}
+</style>
+
